e9f9d63e54264a3c43775ddd0c36b0af49d59cdd
Franco Silva <francosilva@Francos-MacBook-Pro.local>
2020-02-03 13:58:29 -0600

    commit

---
+++ apps.js
@@ -0,0 +1,65 @@
+// name = "Franco"; 
+// var name;
+//let name; 
+// console.log(name); 
+
+// setName();
+// function setName() {
+//     var name = 'Franco'; 
+//     consol
+//     e.log(name); 
+// }
+
+// 
+
+// console.log('some text');
+// let avg = findAvg(2, 2);
+// console.log('some text', avg);
+// function findAvg(a, b) {
+//     console.log('some text');
+//     var answer = ( a + b) / 2;
+//     return answer;
+// }
+
+// let fruits = ['apple', 'banana', 'orange']; 
+// //let favFruits; 
+// function firstFruit () {
+//     let favFruits = fruits[1];
+//     console.log(fruits[0]);
+
+//     function printFavFruits() {
+//         console.log(favFruits); 
+//      }
+//      printFavFruits();
+//      let lesFavFruits;
+//      lesFavFruits = fruits[2]; 
+
+//      console.log(lesFavFruits); 
+
+
+// } 
+
+// firstFruit(); 
+
+// let lesFavFruits;
+// lesFavFruits = fruits[2]; 
+
+Me();
+function Me() {
+    let name ="Franco";
+    console.log('Hello ' + name); 
+}
+//a(asdfgh); 
+let a = function(message) {
+    alert(message);
+} 
+a("asdfgh");
+
+
+// console.log(lesFavFruits); 
+
+//printFavFruits();
+// function printFavFruits() {
+//    console.log(favFruits); 
+// }
+ 
